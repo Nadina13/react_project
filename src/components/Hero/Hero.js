25e66bc11475dc6b98bb60edc23bf940424272ea
Nadina Milewska <milewska.nadina@gmail.com>
2020-12-12 12:02:13 +0100

    15_3

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Hero.scss';
+import PropTypes from 'prop-types';
+
+const Hero = props => (
+    <header className={styles.component}>
+        <h2 className={styles.title}>{props.titleText}</h2>
+        <img src="images/space.png" className={styles.image}></img>
+    </header>
+);
+
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+  };
+
+export default Hero;
---
+++ src/components/List/List.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Hero from '../Hero/Hero';
+import styles from './List.scss';
+import PropTypes from 'prop-types';
+
+
+class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+    }
+
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    }
+
+    render() {
+        return (
+            <section className={styles.component}>
+                <Hero titleText={this.props.title} />
+                <div className={styles.description}>
+                    {this.props.children}
+                </div>
+            </section>
+        )
+    }
+}
+
+export default List;
