efa735ccbfee593a3e1835c6fee36c2966a42e35
Nadina Milewska <milewska.nadina@gmail.com>
2020-12-13 17:55:40 +0100

    Task 15_4

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from '../Hero/Hero';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 
 class List extends React.Component {
     static propTypes = {
         title: PropTypes.node.isRequired,
         children: PropTypes.node.isRequired,
+        image: PropTypes.node.isRequired,
     }
 
     static defaultProps = {
@@ -17,9 +19,14 @@ class List extends React.Component {
     render() {
         return (
             <section className={styles.component}>
-                <Hero titleText={this.props.title} />
+                <Hero titleText={this.props.title} imageMain={this.props.image} />
                 <div className={styles.description}>
                     {this.props.children}
+                </div>
+                <div className={styles.columns}>
+                    <Column columnOne={'Animals'}></Column>
+                    <Column columnTwo={'Plants'}></Column>
+                    <Column columnThree={'Minerals'}></Column>
                 </div>
             </section>
         )
