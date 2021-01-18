338993cf699690ab98281599debd80160cd111e6
Nadina Milewska <milewska.nadina@gmail.com>
2021-01-18 19:32:56 +0100

    Task 17_4_done

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,7 +5,7 @@ import { settings } from '../../data/dataStore';
 
 const Info = () => (
   <Container>
-    <Hero titleText={'Info'} imageMain={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}></Hero>
+    <Hero titleText={'Info'} imageMain={settings.info.image} />
     <h2>{settings.info.pageTitle}</h2>
     <p>{settings.info.pageContent}</p>
   </Container>
--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -3,7 +3,7 @@ import SearchResults from './SearchResults';
 import { getCardsForSearch } from '../../redux/cardsRedux.js';
 
 const mapStateToProps = (state, props) => ({
-  cards: getCardsForSearch(state, props.id),
+  cards: getCardsForSearch(state, props.match.params.searchString), 
 });
 
 export default connect(mapStateToProps)(SearchResults);
